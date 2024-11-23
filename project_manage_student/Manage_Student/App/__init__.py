b42cb8bda32b285b0cb92aa50899fbbf927a7b48
Cnatro72 <nguyenhoanglongnhat72@gmail.com>
2024-11-24 01:09:56 +0700

    'update-24-11'

---
+++ project_manage_student/Manage_Student/App/__init__.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from urllib.parse import quote
+
+app = Flask(__name__)
+#connect database
+app.config["SQLALCHEMY_DATABASE_URI"] ="mysql+pymysql://root:%s@localhost/manage_student?charset=utf8mb4" %quote("Admin123@")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
+
+db = SQLAlchemy(app)
