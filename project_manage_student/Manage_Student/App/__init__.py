1bd51420fc8345d05433f742a9982b7a6944f8e0
Cnatro72 <nguyenhoanglongnhat72@gmail.com>
2024-11-28 18:12:57 +0700

    'update-28-11'

--- project_manage_student/Manage_Student/App/__init__.py
+++ project_manage_student/Manage_Student/App/__init__.py
@@ -4,7 +4,7 @@ from urllib.parse import quote
 
 app = Flask(__name__)
 #connect database
-app.config["SQLALCHEMY_DATABASE_URI"] ="mysql+pymysql://root:%s@localhost/manage_student?charset=utf8mb4" %quote("Admin123@")
+app.config["SQLALCHEMY_DATABASE_URI"] ="mysql+pymysql://root:%s@localhost/managestudent?charset=utf8mb4" %quote("Admin123@")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 
 db = SQLAlchemy(app)
--- project_manage_student/Manage_Student/App/model_Example.py
+++ project_manage_student/Manage_Student/App/model_Example.py
@@ -7,10 +7,19 @@ from sqlalchemy.orm import relationship
 from sqlalchemy import func
 
 
+# class Phone(db.Model):
+#     id = Column(Integer,autoincrement=True,primary_key=True)
+#     number = Column(String(10),unique=True)
 class UserRole(enum.Enum):
     TEACHER = 1
     STAFF = 2
     ADMIN = 3
+
+
+class Title(enum.Enum):
+    BACHELOR = 1
+    MASTER = 2
+    DOCTRIBER = 3
 
 
 class Grade(enum.Enum):
@@ -43,21 +52,30 @@ class User(db.Model):
     last_login = Column(DateTime)
 
     profile = relationship('Profile',backref='User',lazy=True,uselist=False)
-    # staff
-    students = relationship('Student',backref='User',lazy=True)
-    staff_classes = relationship('StaffClass',backref='User',lazy=True)
-    # teacher
-    classes = relationship('Class',backref='User',lazy=True)
-    # admin
-    regulations = relationship('Regulation',backref='User',lazy=True)
 
+
+class Teacher(db.Model):
+    id = Column(Integer, ForeignKey(User.id),primary_key=True)
+    title = Column(Enum(Title))
+
+    user = relationship('User',backref='Teacher',lazy=True, uselist=False)
+    classes = relationship('Class',backref='Teacher',lazy=True)
+    subjects = relationship('TeacherSubject', backref='Teacher', lazy=True)
+
+
+class Staff(db.Model):
+    id = Column(Integer,ForeignKey(User.id),primary_key=True)
+
+    user = relationship('User',backref='Staff',lazy=True,uselist=False)
+    students = relationship('Student',backref='Staff',lazy=True)
+    classes = relationship('StaffClass', backref='Staff',lazy=True)
 
 
 class Student(db.Model):
     id = Column(Integer,ForeignKey(Profile.id), primary_key=True)
     status_payment = Column(Boolean,default=False)
 
-    staff_id = Column(Integer, ForeignKey(User.id),nullable=False)
+    staff_id = Column(Integer, ForeignKey(Staff.id),nullable=False)
 
     profile = relationship('Profile',backref='Student',lazy=True, uselist=False)
     classes = relationship('StudentClass',backref='Student',lazy=True)
@@ -70,7 +88,7 @@ class Class(db.Model):
     quantity_student = Column(Integer)
     grade = Column(Enum(Grade))
 
-    teacher_id = Column(Integer,ForeignKey(User.id),nullable=False)
+    teacher_id = Column(Integer,ForeignKey(Teacher.id),nullable=False)
 
     students = relationship('StudentClass',backref='Class',lazy=True)
     staffs = relationship('StaffClass', backref='Class', lazy=True)
@@ -105,14 +123,14 @@ class StudentClass(db.Model):
 class StaffClass(db.Model):
     id = Column(Integer,autoincrement=True,primary_key=True)
 
-    staff_id = Column(Integer,ForeignKey(User.id), nullable=False)
+    staff_id = Column(Integer,ForeignKey(Staff.id), nullable=False)
     class_id = Column(Integer,ForeignKey(Class.id), nullable = False)
 
 
 class TeacherSubject(db.Model):
     id = Column(Integer, autoincrement= True, primary_key=True)
 
-    teacher_id = Column(Integer, ForeignKey(User.id), nullable= False)
+    teacher_id = Column(Integer, ForeignKey(Teacher.id), nullable= False)
     subject_id  = Column(Integer, ForeignKey(Subject.id), nullable= False)
 
     teacher_plans = relationship('TeacherPlan', backref='TeacherSubject', lazy=True)
@@ -143,16 +161,6 @@ class Score(db.Model):
     )
 
 
-class Regulation(db.Model):
-    id = Column(Integer,primary_key=True,autoincrement=True)
-    type = Column(String(50))
-    regulation_name = Column(String(100))
-    min = Column(Integer)
-    max = Column(Integer)
-
-    admin_id = Column(Integer, ForeignKey(User.id), nullable=False)
-
-
 if __name__ == '__main__':
     with app.app_context():
         # db.create_all()
@@ -165,11 +173,17 @@ if __name__ == '__main__':
         # db.session.commit()
 
         #create account
-        admin = User(id=profile_1.id,username="Cnatro",password=str(hashlib.md5('Admin123@'.encode('utf-8')).hexdigest()),user_role=UserRole.ADMIN)
+        acc1 = User(id=profile_1.id,username="Cnatro",password=str(hashlib.md5('Admin123@'.encode('utf-8')).hexdigest()),user_role=UserRole.ADMIN)
         acc2 = User(id=profile_2.id,username="NhanTran",password=str(hashlib.md5('Nhan123@'.encode('utf-8')).hexdigest()), user_role=UserRole.TEACHER)
         acc3 = User(id=profile_3.id,username="NgocThai",password=str(hashlib.md5('Thai123@'.encode('utf-8')).hexdigest()), user_role=UserRole.STAFF)
 
         # db.session.add_all([acc1,acc2,acc3])
+        # db.session.commit()
+
+        # create staff
+        staff = Staff(id=profile_3.id)
+
+        # db.session.add(staff)
         # db.session.commit()
 
         # create teacher
@@ -183,18 +197,28 @@ if __name__ == '__main__':
         # db.session.add_all([p4,p5,p6,p7,p8,p9])
         # db.session.commit()
 
-        teacher4 = User(id=p4.id,username="Gv4",password=str(hashlib.md5('Gv4123@'.encode('utf-8')).hexdigest()),
+        acc4 = User(id=p4.id,username="Gv4",password=str(hashlib.md5('Gv4123@'.encode('utf-8')).hexdigest()),
                     user_role=UserRole.TEACHER)
-        teacher5 = User(id=p5.id, username="Gv5", password=str(hashlib.md5('Gv5123@'.encode('utf-8')).hexdigest()),
+        acc5 = User(id=p5.id, username="Gv5", password=str(hashlib.md5('Gv5123@'.encode('utf-8')).hexdigest()),
                     user_role=UserRole.TEACHER)
-        teacher6 = User(id=p6.id, username="Gv6", password=str(hashlib.md5('Gv6123@'.encode('utf-8')).hexdigest()),
+        acc6 = User(id=p6.id, username="Gv6", password=str(hashlib.md5('Gv6123@'.encode('utf-8')).hexdigest()),
                     user_role=UserRole.TEACHER)
-        teacher7 = User(id=p7.id, username="Gv7", password=str(hashlib.md5('Gv7123@'.encode('utf-8')).hexdigest()),
+        acc7 = User(id=p7.id, username="Gv7", password=str(hashlib.md5('Gv7123@'.encode('utf-8')).hexdigest()),
                     user_role=UserRole.TEACHER)
-        teacher8 = User(id=p8.id, username="Gv8", password=str(hashlib.md5('Gv8123@'.encode('utf-8')).hexdigest()),
+        acc8 = User(id=p8.id, username="Gv8", password=str(hashlib.md5('Gv8123@'.encode('utf-8')).hexdigest()),
                     user_role=UserRole.TEACHER)
-        teacher9 = User(id=p9.id, username="Gv9", password=str(hashlib.md5('Gv9123@'.encode('utf-8')).hexdigest()),
+        acc9 = User(id=p9.id, username="Gv9", password=str(hashlib.md5('Gv9123@'.encode('utf-8')).hexdigest()),
                     user_role=UserRole.TEACHER)
+
+        # db.session.add_all([acc4, acc5, acc6, acc7, acc8, acc9])
+        # db.session.commit()
+
+        teacher4 = Teacher(id=acc4.id,title=Title.DOCTRIBER)
+        teacher5 = Teacher(id=acc5.id, title=Title.MASTER)
+        teacher6 = Teacher(id=acc6.id, title=Title.BACHELOR)
+        teacher7 = Teacher(id=acc7.id, title=Title.DOCTRIBER)
+        teacher8 = Teacher(id=acc8.id, title=Title.MASTER)
+        teacher9 = Teacher(id=acc9.id, title=Title.DOCTRIBER)
 
         # db.session.add_all([teacher4, teacher5, teacher6, teacher7, teacher8, teacher9])
         # db.session.commit()
@@ -269,13 +293,4 @@ if __name__ == '__main__':
         ]
         # for s in semesters:
         #     db.session.add(s)
-        # db.session.commit()
-
-        regulations = [
-            Regulation(type="Student",regulation_name="Số tuổi của học sinh", min=15, max=18,admin_id=admin.id),
-            Regulation(type="Quantity_student",regulation_name="Sỉ số lớp học",min=10,max=40,admin_id=admin.id)
-        ]
-
-        # for regulation in regulations:
-        #     db.session.add(regulation)
         # db.session.commit()
